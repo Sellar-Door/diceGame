f0139ab0fbdba4329b730d5c74b057b669e2080b
CN0073 <cn0073@CN0073.lan>
2019-06-11 09:10:50 +0100

    single player dice game now complete

--- script.js
+++ script.js
@@ -1,25 +1,31 @@
+let totalScore = 0
 
 const rollDice = () => {
 let d1 = Math.floor(Math.random()*6+1)
-total = d1
-die1.innerHTML = d1 
+let total = d1
 let image = document.getElementById("image")
-document.getElementById("score").addEventListener("click", function(event){ 
-   
-})
-
+totalScore = totalScore + total
+die1.innerHTML = "You rolled " + d1
+score.innerHTML = "TOTAL "+ totalScore
+if(totalScore >= 20) {
+   score.innerHTML = "YOU WIN! You scored a total of " +totalScore
+} if(d1 === 1) {
+   totalScore = totalScore - totalScore
+   score.innerHTML = "GAME OVER. Roll the dice to start again"
+} if(totalScore >=20) {
+   totalScore = totalScore - totalScore
+}
 if(d1 == 1) {
-    image.src = "img/dice1.png"
+   image.src = "img/dice1.png"
 } if(d1 == 2) {
-    image.src = "img/dice2.png"
+   image.src = "img/dice2.png"
 } if(d1 == 3) {
-    image.src = "img/dice3.png"
+   image.src = "img/dice3.png"
 } if(d1 == 4) {
-    image.src = "img/dice4.png"
+   image.src = "img/dice4.png"
 } if(d1 == 5) {
-    image.src = "img/dice5.png"
+   image.src = "img/dice5.png"
 } if(d1 == 6) {
-    image.src = "img/dice6.png"
+   image.src = "img/dice6.png"
 }
 }
-
